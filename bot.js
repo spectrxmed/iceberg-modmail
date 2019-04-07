ce27f59fbcb7251e3f1d08e4176e2e2e499603a2
spectrxmed <49282781+spectrxmed@users.noreply.github.com>
2019-04-07 14:12:16 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,15 +1,15 @@
 const Discord = require('discord.js');
-const.client = newDiscord.Client();
+const client = new Discord.Client();
 
-client.on('ready' () => {
-  console.log('I am ready!');
+client.on('ready', () => {
+    console.log('I am ready!');
 });
- 
- cleint.on('message', message => {
-  if (message.content === 'ping'){
-    message.reply('pong');
-   }
-  });
-  
-  // THIS MUST BE THIS WAY
-  client.login(process.env.BOT_TOKEN);
+
+client.on('message', message => {
+    if (message.content === 'ping') {
+    	message.reply('pong');
+  	}
+});
+
+// THIS  MUST  BE  THIS  WAY
+client.login(process.env.BOT_TOKEN);
