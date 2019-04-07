0e79a2ab19c886f951da5f47a0691384e472d273
spectrxmed <49282781+spectrxmed@users.noreply.github.com>
2019-04-07 21:41:53 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -12,4 +12,4 @@ client.on('message', message => {
 });
 
 // THIS  MUST  BE  THIS  WAY
-client.login(process.env.BOT_TOKEN);
+client.login(process.env.NTYzMDIwNTcwNjgyNDU4MTEy.XKmQ0A.PgJxCHIvmFW5Y0ejrwZzbZoN7QQ);
